678b410a27b3c8a01967c2f0f22d7535e6aa0854
wladi <wladimir2000@protonmail.com>
2023-11-25 16:26:32 -0300

    UTF8, Diferents colors for pair and unpair lines.

--- docker_color_posh.psm1
+++ docker_color_posh.psm1
@@ -1,3 +1,9 @@
+# Set the encoding to UTF8 to avoid problems with special characters
+$OutputEncoding = [Console]::OutputEncoding = [Text.UTF8Encoding]::UTF8
+
+# This function colorize the output of the docker command
+# This function is called by the DockerColorPosh function
+# This function is not intended to be called directly
 function ColorizeOutput {
     Param(
         [Parameter(ValueFromPipeline=$true)]
@@ -5,30 +11,44 @@ function ColorizeOutput {
     )
 
     Begin {
-        # Inicializar un array vacío al empezar a recibir objetos del pipeline
+        # Create an array to store each line of the docker command output
         $array_lines = @()
     }
 
     Process {
-        # Añadir cada línea del input al array
+        # Add each line to the array
         $array_lines += $InputLine
     }
 
     End {
         for ($i = 0; $i -lt $array_lines.Count; $i++) {
+            # if the line is the first one, colorize it with yellow
             if ($i -eq 0) {
                 Write-Host $array_lines[$i] -ForegroundColor Yellow
                 continue
             }
-            Write-Host "$($array_lines[$i])" -ForegroundColor Cyan
+            # if the line is pair, colorize it better readability
+            if ($i % 2 -eq 0) {
+                Write-Host $($array_lines[$i]) -ForegroundColor DarkCyan
+                continue
+            }
+            # if the line is impair, colorize it better readability
+            Write-Host $($array_lines[$i]) -ForegroundColor Cyan
         }
     }
 }
-# chance the format
 
+# This function is the main function of the module
+# Is the only function exported by the module
+# It´s for call the docker command and validate if the command is in the compatible list of commands and then call the ColorizeOutput function
+# If the command is not in the list, the output will be shown without color
 function DockerColorPosh {
     Begin {
         $entire_command = "docker"
+        # This regex validate if the command is supported by the module.
+        # If the command is not supported, the output will be shown without color
+        # If you want to add more commands, add the command name to the regex
+        # The regex accept the command with or without arguments
         $avaliable_commands_regex = "^docker (ps|images)(\s+.+)?$"
         }
     Process {
@@ -46,3 +66,6 @@ function DockerColorPosh {
 
     }
 }
+
+# Export only the DockerColorPosh function
+Export-ModuleMember -Function 'DockerColorPosh'
