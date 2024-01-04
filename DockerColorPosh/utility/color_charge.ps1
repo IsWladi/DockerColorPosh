16a93cc0975d252fd206cea92cf86d80a501a41e
wladi <wladimir2000@protonmail.com>
2024-01-03 23:18:42 -0300

    Add error handling for invalid file paths and theme names.

--- DockerColorPosh/utility/color_charge.ps1
+++ DockerColorPosh/utility/color_charge.ps1
@@ -4,13 +4,11 @@ $colorSchemeFilePath = "$PSScriptRoot\..\presets\colors.json"
 
 # Path to the target color scheme file (of the module or custom if exists)
 $targetColorSchemeFilePath = if ($null -eq $dcpCustomColorSchemePath) {$colorSchemeFilePath} else {$dcpCustomColorSchemePath}
-Write-Warning "targetColorSchemeFilePath: $targetColorSchemeFilePath"
 
 # by default, use the DEFAULT color scheme
 # if the user has set a color scheme, use it
 # The $dcpColorScheme variable applies to both the module and custom color schemes, based on $targetColorSchemeFilePath.
 $colorScheme = if ($null -eq $dcpColorScheme) { "DARK" } else { $dcpColorScheme }
-Write-Warning "colorScheme: $colorScheme"
 
 # Function to convert the color in text to ConsoleColor
 function Convert-ToConsoleColor($colorName) {
@@ -24,38 +22,55 @@ function Convert-ToConsoleColor($colorName) {
 }
 
 # Charge the configuration from the JSON file
-$config = Get-Content -Path $targetColorSchemeFilePath | ConvertFrom-Json
+$SchemeConfigFile = $null
+
+# Check if the color scheme file exists
+$useDefaultColorScheme = $false
+if (Test-Path -Path $targetColorSchemeFilePath) {
+    $SchemeConfigFile = Get-Content -Path $targetColorSchemeFilePath | ConvertFrom-Json
+} else {
+    Write-Warning "Color scheme file not found. Using default configuration file instead."
+    $SchemeConfigFile = Get-Content -Path $colorSchemeFilePath | ConvertFrom-Json
+    $useDefaultColorScheme = $true
+}
 
 # Check if the color scheme is well defined
 # Get the list of color schemes
 $colorSchemesList = @()
-    foreach ($scheme in $config.ColorSchemes.PSObject.Properties.Name) {
-        $colorSchemesList += $scheme
-    }
+
+foreach ($scheme in $SchemeConfigFile.ColorSchemes.PSObject.Properties.Name) {
+    $colorSchemesList += $scheme
+}
 # validate the color scheme with the list of color schemes
 if ($null -ne $dcpColorScheme -and $colorSchemesList -notcontains $dcpColorScheme  ) {
-    Write-Warning "Color scheme '$dcpColorScheme' not found in the $targetColorSchemeFilePath file. Using default configuration instead."
-        $config = Get-Content -Path $colorSchemeFilePath | ConvertFrom-Json
-        $colorScheme = "DARK"
+    if ($useDefaultColorScheme) {
+        Write-Warning "Theme '$dcpColorScheme' not found in the default scheme configuration file. Using 'DARK' theme instead."
+    }
+    else {
+        Write-Warning "Theme '$dcpColorScheme' not found in the $targetColorSchemeFilePath custom configuration file. Using default configuration instead."
+    }
+
+    $SchemeConfigFile = Get-Content -Path $colorSchemeFilePath | ConvertFrom-Json
+    $colorScheme = "DARK"
 }
 
-$config = $config.ColorSchemes.$colorScheme
+$SchemeConfigFile = $SchemeConfigFile.ColorSchemes.$colorScheme
 
 # Assign colors
 #
 # General colors
-$main_color = Convert-ToConsoleColor $config.GeneralColors.MainColor
-$secondary_color = Convert-ToConsoleColor $config.GeneralColors.SecondaryColor
-$header_color = Convert-ToConsoleColor $config.GeneralColors.HeaderColor
+$main_color = Convert-ToConsoleColor $SchemeConfigFile.GeneralColors.MainColor
+$secondary_color = Convert-ToConsoleColor $SchemeConfigFile.GeneralColors.SecondaryColor
+$header_color = Convert-ToConsoleColor $SchemeConfigFile.GeneralColors.HeaderColor
 #
 # Help colors
-$help_flag_color = Convert-ToConsoleColor $config.HelpTypeCommands.HelpFlagColor
-$help_header_color = Convert-ToConsoleColor $config.HelpTypeCommands.HelpHeaderColor
+$help_flag_color = Convert-ToConsoleColor $SchemeConfigFile.HelpTypeCommands.HelpFlagColor
+$help_header_color = Convert-ToConsoleColor $SchemeConfigFile.HelpTypeCommands.HelpHeaderColor
 #
 # List colors
 ## Container list colors
-$container_status_up_color = Convert-ToConsoleColor $config.ListTypeCommands.ContainerListSubcommands.Up
-$container_status_exited_color = Convert-ToConsoleColor $config.ListTypeCommands.ContainerListSubcommands.Exited
+$container_status_up_color = Convert-ToConsoleColor $SchemeConfigFile.ListTypeCommands.ContainerListSubcommands.Up
+$container_status_exited_color = Convert-ToConsoleColor $SchemeConfigFile.ListTypeCommands.ContainerListSubcommands.Exited
 $containerStatusColors = @{
     "Up" = $container_status_up_color
     "Exited" = $container_status_exited_color
